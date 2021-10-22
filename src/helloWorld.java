6cf43dc3e99fd8fba5de313ac8e4acd769a09999
KankikaJoy <ks20prag@gmail.com>
2021-10-21 20:56:18 -0400

    first commit

---
+++ src/helloWorld.java
@@ -0,0 +1,22 @@
+public class helloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("HELLO WORLD");
+        
+
+     /*   int a=10;
+        int b=20;
+
+
+        if(a>=b){
+            System.out.println("A is greatest");
+        }
+        else{
+            System.out.println("B is greatest");
+        }*/
+
+
+
+    }
+
+}
