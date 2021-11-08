6076d441062009dd79548359ba645d053f299539
KankikaJoy <ks20prag@gmail.com>
2021-11-07 20:59:54 -0500

    while loop example

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here Print 1-10 using while
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
